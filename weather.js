57998824e453314fde8a31ab9cd1578a9f71b837
Clarity-89 <homes89@ukr.net>
2015-08-05 17:50:13 +0300

    Fixed css background image size, added loader img

--- weather.js
+++ weather.js
@@ -34,21 +34,22 @@ $(document).ready(function () {
                     // Change background image based on the time of a day and weather conditions
                     var time = new Date().getHours();
                     console.log(time);
-                   if (time < 5 || time >= 22){
+                    if (time < 5 || time >= 22) {
                         $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/moon.jpg')");
                     } else {
-                        if (data.weather[0].icon == '50d'){
+                        if (data.weather[0].icon == '50d') {
                             $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/fog.jpg')");
-                        } else if (data.weather[0].icon == '02d' || data.weather[0].icon == '03d' || data.weather[0].icon == '04d'){
+                        } else if (data.weather[0].icon == '02d' || data.weather[0].icon == '03d' || data.weather[0].icon == '04d') {
                             $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/cloudy1.jpg')");
-                        } else if (data.weather[0].icon == '09d' || data.weather[0].icon == '10d'){
+                        } else if (data.weather[0].icon == '09d' || data.weather[0].icon == '10d') {
                             $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/rain.jpg')");
-                        } else if (data.weather[0].icon == '13d'){
+                        } else if (data.weather[0].icon == '13d') {
                             $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/snow.jpg')");
                         } else {
                             $(".container").css("background-image", "url('https://dl.dropboxusercontent.com/u/28151607/backgrounds/sun.jpg')");
                         }
                     }
+                    $(".loading").fadeOut("slow");
 
                 })
                 .fail(function () {
@@ -75,11 +76,12 @@ $(document).ready(function () {
     }
 
     // Convert Celcius to Fahrenheit + round to integer
-    function celcToFahr(celcius){
-        return (celcius * (9/5) + 32).toFixed(0);
+    function celcToFahr(celcius) {
+        return (celcius * (9 / 5) + 32).toFixed(0);
     }
 
     var c = $('#inner_cont');
 
     showWeather(c);
+
 })
